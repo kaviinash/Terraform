192db5641daa1deeb4bebf12c82597ac23862e17
Avinash_Devops <kaviinash1@gmail.com>
2026-04-04 22:11:37 +0530

    Fix backend configuration in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
 terraform {
+  backend "azurerm"{}
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.47.0"
     }
-    backend "azurerm"{
 
-    }
   }
 }
 
