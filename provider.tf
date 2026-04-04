154e9c6237c7ce72185183066381adfafcf2efea
Avinash_Devops <kaviinash1@gmail.com>
2026-04-04 22:07:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source = "hashicorp/azurerm"
+      version = "4.47.0"
+    }
+    backend "azurerm"{
+
     }
   }
-
-  backend "azurerm" {
-
 }
 
 provider "azurerm" {
-  features {}
+    features {
+      
+    }
+
+  # Configuration options
 }
