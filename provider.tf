303adac47c831a6dc1992066e6c9a3fbe9f7cfa4
Avinash_Devops <kaviinash1@gmail.com>
2026-04-04 21:57:38 +0530

    Update azurerm provider configuration
    Removed version specification and subscription ID from azurerm provider configuration.

--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
 terraform {
   required_providers {
+     backend "azurerm"{
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.32.0"
+
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
-  subscription_id = "db9c4b37-5f23-4e02-96a8-89a41ddf29a5"
+
   features {
     
   }
